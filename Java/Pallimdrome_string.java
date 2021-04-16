97e815ac74a52ebc9615a70b6796dcd55de4d1b9
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-04-16 23:34:54 +0530

    Create Pallimdrome_string.java

---
+++ Java/Pallimdrome_string.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int n=A.length(),count=0;
+        for(int i=0;i<n/2;i++){
+            if(A.charAt(i)==A.charAt(n-1-i))
+                count++;
+        }
+        if(count==n/2)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
+
+
