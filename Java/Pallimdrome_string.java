fa4f10d7a28830f7e6c1a1e5aa6ebd3ad0a8328b
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-05-11 23:20:41 +0530

    Update Pallimdrome_string.java

--- Java/Pallimdrome_string.java
+++ Java/Pallimdrome_string.java
@@ -7,7 +7,6 @@ public class Solution {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
         int n=A.length(),count=0;
         for(int i=0;i<n/2;i++){
             if(A.charAt(i)==A.charAt(n-1-i))
